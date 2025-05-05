0433b1630438603c7df73478c67627d7eea7596d
kcconsigo <kcconsigo@gmail.com>
2025-05-05 12:44:21 +0800

    modified: resolved web locator on pim locators editEmplistbtn: causing to wait for locator because of invalid locator

--- weblocators/pimuserlocators.ts
+++ weblocators/pimuserlocators.ts
@@ -2,17 +2,17 @@
 export const pimlocators = {
     pimmenu : ".oxd-main-menu-item",
     addempTab : ".oxd-topbar-body-nav-tab",
-    addEmpFirstName: ".orangehrm-firstname",
-    addEmpMidName: ".orangehrm-middlename",
-    addEmpLastName: ".orangehrm-lastname",
-    addEmpID: ".oxd-input",
-    addEmpbuttonSave: "//button[@type='submit']",
-    addEmpNationalityField: ".oxd-select-text.oxd-select-text--active",
-    addEmpMaritalStatusField: ".oxd-select-text.oxd-select-text--active",
-    addEmpInfoSave: "//button[@type='submit']",
+    EmpFirstName: ".orangehrm-firstname",
+    EmpMidName: ".orangehrm-middlename",
+    EmpLastName: ".orangehrm-lastname",
+    EmpID: ".oxd-input",
+    EmpbuttonSave: "//button[@type='submit']",
+    EmpNationalityField: ".oxd-select-text.oxd-select-text--active",
+    EmpMaritalStatusField: ".oxd-select-text.oxd-select-text--active",
+    EmpInfoSave: "//div[@class='orangehrm-custom-fields']//button[@type='submit'][normalize-space()='Save']",
     listEmpNavTab: ".oxd-topbar-body-nav-tab",
     listEmployeeName: ".oxd-autocomplete-text-input",
     listEmployeeNameSelect: ".oxd-autocomplete-dropdown", 
     listEmpSearchbtn: "//button[@type='submit']",
-    editEmplistbtn: ".oxd-icon-button"
+    editEmplistbtn: "//div[@class='orangehrm-container']//button[@type='button']"
 }
